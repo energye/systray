8a0b2b7191a5a01f19a91c28ee3e7f066aa07c75
杨红岩 <snxamdf@126.com>
2023-01-24 19:33:33 +0800

    function declaration

--- systray_other.go
+++ systray_other.go
@@ -4,15 +4,15 @@
 package systray
 
 // CreateMenu 创建托盘菜单, 如果托盘菜单是空, 把菜单项添加到托盘
-// 该法主动调用后 鼠标事件失效
+// 该方法主动调用后 如果托盘菜单已创建则添加进去, 之后鼠标事件失效
 //
-// MacOSX平台
+// 仅MacOSX平台
 func CreateMenu() {
 }
 
 // SetMenuNil 托盘菜单设置为nil, 如果托盘菜单不是空, 把菜单项设置为nil
-// 该方法主动调用后 鼠标事件生效
+// 该方法主动调用后 将移除托盘菜单, 之后鼠标事件生效
 //
-// MacOSX平台
+// 仅MacOSX平台
 func SetMenuNil() {
 }
